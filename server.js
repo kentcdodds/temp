0e15b3b41e201e17eda8b9f93a68e8938c7286a1
Kent C. Dodds <kent@doddsfamily.us>
2014-08-27 19:11:26 -0600

    Step 2

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var faker = require('faker');
+var cors = require('cors');
+
+var app = express();
+
+app.use(cors());
+
+app.get('/random-user', function(req, res) {
+  var user = faker.Helpers.userCard();
+  user.avatar = faker.Image.avatar();
+  res.json(user);
+});
+
+app.listen(3000, function() {
+  console.log('App listening on localhost:3000');
+});
