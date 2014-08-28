635492b901447d553be35c170ef18c276a90de96
Kent C. Dodds <kent@doddsfamily.us>
2014-08-27 19:55:31 -0600

    Step 3

--- public/app.js
+++ public/app.js
@@ -4,16 +4,27 @@
 
 	app.constant('API_URL', 'http://localhost:3000');
 
-	app.controller('MainCtrl', function MainCtrl(RandomUserFactory) {
+	app.controller('MainCtrl', function MainCtrl(RandomUserFactory, UserFactory) {
 		'use strict';
 		var vm = this;
 		vm.getRandomUser = getRandomUser;
+    vm.login = login;
 
 		function getRandomUser() {
 			RandomUserFactory.getUser().then(function success(response) {
 				vm.randomUser = response.data;
-			});
+			}, handleError);
 		}
+
+    function login(username, password) {
+      UserFactory.login(username, password).then(function success(response) {
+        vm.user = response.data;
+      }, handleError);
+    }
+
+    function handleError(response) {
+      alert('Error: ' + response.data);
+    }
 	});
 
 	app.factory('RandomUserFactory', function RandomUserFactory($http, API_URL) {
@@ -26,4 +37,18 @@
 			return $http.get(API_URL + '/random-user');
 		}
 	});
+
+  app.factory('UserFactory', function UserFactory($http, API_URL) {
+    'use strict';
+    return {
+      login: login
+    };
+
+    function login(username, password) {
+      return $http.post(API_URL + '/login', {
+        username: username,
+        password: password
+      });
+    }
+  });
 })();
--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var faker = require('faker');
 var cors = require('cors');
+var bodyParser = require('body-parser');
+
+var user = {
+  username: 'kentcdodds',
+  password: 'p'
+};
 
 var app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/login', authenticate, function(req, res) {
+  res.json(user);
+});
 
 app.get('/random-user', function(req, res) {
   var user = faker.Helpers.userCard();
@@ -15,3 +26,16 @@ app.get('/random-user', function(req, res) {
 app.listen(3000, function() {
   console.log('App listening on localhost:3000');
 });
+
+// UTIL FUNCTIONS
+
+function authenticate(req, res, next) {
+  var body = req.body;
+  if (!body.username || !body.password) {
+    return res.status(400).end('Must provide username and password');
+  }
+  if (body.username !== user.username || body.password !== user.password) {
+    return res.status(401).end('Username or password incorrect');
+  }
+  next();
+}
