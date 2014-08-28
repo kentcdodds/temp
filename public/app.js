10bd7480b2472f35d0876e28b8c39a7432936f61
Kent C. Dodds <kent@doddsfamily.us>
2014-08-27 20:13:49 -0600

    Step 5

--- public/app.js
+++ public/app.js
@@ -1,6 +1,8 @@
 (function() {
   'use strict';
-	var app = angular.module('app', []);
+	var app = angular.module('app', [], function($httpProvider) {
+    $httpProvider.interceptors.push('AuthInterceptor');
+  });
 
 	app.constant('API_URL', 'http://localhost:3000');
 
@@ -38,7 +40,7 @@
 		}
 	});
 
-  app.factory('UserFactory', function UserFactory($http, API_URL) {
+  app.factory('UserFactory', function UserFactory($http, API_URL, AuthTokenFactory) {
     'use strict';
     return {
       login: login
@@ -48,7 +50,48 @@
       return $http.post(API_URL + '/login', {
         username: username,
         password: password
+      }).then(function success(response) {
+        AuthTokenFactory.setToken(response.data.token);
+        return response;
       });
+    }
+  });
+
+  app.factory('AuthTokenFactory', function AuthTokenFactory($window) {
+    'use strict';
+    var store = $window.localStorage;
+    var key = 'auth-token';
+    return {
+      getToken: getToken,
+      setToken: setToken
+    };
+
+    function getToken() {
+      return store.getItem(key);
+    }
+
+    function setToken(token) {
+      if (token) {
+        store.setItem(key, token);
+      } else {
+        store.removeItem(key);
+      }
+    }
+  });
+
+  app.factory('AuthInterceptor', function AuthInterceptor(AuthTokenFactory) {
+    'use strict';
+    return {
+      request: addAuthToken
+    };
+
+    function addAuthToken(config) {
+      var token = AuthTokenFactory.getToken();
+      if (token) {
+        config.headers = config.headers || {};
+        config.headers.Authorization = 'Bearer ' + token;
+      }
+      return config;
     }
   });
 })();
